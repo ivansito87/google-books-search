578b9e54e089764fc22b8b4fe6e86fc182290f72
Ivan Rendon <ivansito87@gmail.com>
2019-06-15 22:17:58 -0400

    adds model and connections to db as well as all the routing needed for project

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,1 +1,14 @@
 const db = require("../models");
+
+// Defining the methods for the book controllers
+module.exports = {
+    findAll: (req, res) => {
+        db.Book.find(req.query)
+            .then(dbBook => res.json(dbBook))
+            .catch(err => res.status(422).)
+
+    }
+
+};
+
+// find the final product here [here](https://github.com/craigcompton/NYT-Google-Books/blob/master/controllers/bookController.js)
